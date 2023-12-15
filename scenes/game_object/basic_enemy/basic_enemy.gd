c4e3a86a9c6afda26b0eb5dd03ab8b607e42aaf0
hou ian <ikl258794613@gmail.com>
2023-12-15 17:25:45 +0800

    [axe] add axe upgrade

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -4,9 +4,6 @@ const MAX_SPEED = 40
 
 @onready var health_component:HealthComponent = $HealthComponent
 
-
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var direction = get_direction_to_player() 
--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -9,15 +9,6 @@ var current_upgrades = {}
 func _ready():
 	experience_manager.level_up.connect(on_level_up)
 
-func on_level_up(current_level:int):
-	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
-	if chosen_upgrade == null :
-		return
-		
-	var upgrade_screen_instance = upgrade_screen_scne.instantiate()
-	add_child(upgrade_screen_instance)
-	upgrade_screen_instance.set_ability_upgrade([chosen_upgrade] as Array[AbilityUpgrade])
-	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
 
 func apply_upgrade(upgrade:AbilityUpgrade):
 	var has_upgrade = current_upgrades.has(upgrade.id)
@@ -28,8 +19,33 @@ func apply_upgrade(upgrade:AbilityUpgrade):
 		}
 	else:
 		current_upgrades[upgrade.id]["quantity"] +=1 
-	
+		
+	if upgrade.max_quantity > 0 :
+		var current_quantity = current_upgrades[upgrade.id]["quantity"]
+		if current_quantity == upgrade.max_quantity :
+			upgrade_pool = upgrade_pool.filter(func(pool_upgrade) : return pool_upgrade.id != upgrade.id)
+		
 	GameEvents.emit_ability_upgrade_added(upgrade,current_upgrades)
+
+func pick_upgrade():
+	var chosen_upgrades : Array[AbilityUpgrade] = []
+	var filtered_upgrade = upgrade_pool.duplicate()
+	for i in 2 :
+		if filtered_upgrade.size() == 0 :
+			break
+		var chosen_upgrade = filtered_upgrade.pick_random() as AbilityUpgrade
+		
+		chosen_upgrades.append(chosen_upgrade)
+		filtered_upgrade = filtered_upgrade.filter(func (upgrade):return upgrade.id != chosen_upgrade.id)
+	
+	return chosen_upgrades
 
 func on_upgrade_selected(upgrade:AbilityUpgrade):
 	apply_upgrade(upgrade)
+
+func on_level_up(current_level:int):
+	var upgrade_screen_instance = upgrade_screen_scne.instantiate()
+	add_child(upgrade_screen_instance)
+	var chosen_upgrades = pick_upgrade()
+	upgrade_screen_instance.set_ability_upgrade(chosen_upgrades as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
