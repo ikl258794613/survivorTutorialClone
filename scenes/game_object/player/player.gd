ae3100e35196c571bca8b8a4d8fff492771923fd
hou ian <ikl258794613@gmail.com>
2023-12-16 21:25:19 +0800

    [36]  Animating the Player

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -7,6 +7,8 @@ const ACCELERATION_SMOOTHING = 15
 @onready var health_component = $HealthCompentent 
 @onready var health_bar = $HealthBar
 @onready var abilities = $Abilities
+@onready var animation_player = $AnimationPlayer
+@onready var visuals = $Visuals
 
 var number_colliding_bodies = 0
 
@@ -19,12 +21,26 @@ func _ready():
 	update_health_display()
 	
 	
+	
+	
 func _process(delta):
 	var movement_vector = get_movement_vector()
 	var direction = movement_vector.normalized()
 	var target_velocity = direction * MAX_SPEED
 	velocity =  velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
 	move_and_slide()
+	
+	if movement_vector.x != 0 ||movement_vector.y != 0 :
+		animation_player.play("walk")
+	else :
+		animation_player.play("RESET")
+		
+	var move_sign = sign(movement_vector.x)
+	if(move_sign == 0):
+		visuals.scale = Vector2.ONE
+	else:
+		visuals.scale = Vector2(move_sign , 1)
+	
 
 func get_movement_vector():
 	
